be86f31f66d78bd52f6041f43dfc28e9e79279bf
dploop <dploop@163.com>
2017-10-26 18:43:48 +0800

    add cartesian_product function

--- src/ealgo_combinatorics.erl
+++ src/ealgo_combinatorics.erl
@@ -1,4 +1,24 @@
 -module(ealgo_combinatorics).
 -include("ealgo.hrl").
+-export([cartesian_product/2, cartesian_product/1]).
+
+
+%% https://en.wikipedia.org/wiki/Cartesian_product
+%% Gives the cartesian product of list A and list B.
+-spec cartesian_product([term()], [term()]) -> [list()].
+cartesian_product(A, B) when is_list(A), is_list(B) ->
+    [[X, Y] || X <- A, Y <- B].
+
+%% Gives the cartesian product of a list of list.
+-spec cartesian_product([[term()]]) -> [list()].
+cartesian_product([]) ->
+    [];
+cartesian_product([H]) when is_list(H) ->
+    [[X] || X <- H];
+cartesian_product([H | T]) when is_list(H) ->
+    [[X | Y] || X <- H, Y <- cartesian_product(T)].
+
+
+
 
 
