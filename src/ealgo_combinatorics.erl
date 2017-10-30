9f20ad14d82185dc00d22e3d9741edfcafc8b404
dploop <dploop@163.com>
2017-10-30 15:46:41 +0800

    add subsets function

--- src/ealgo_combinatorics.erl
+++ src/ealgo_combinatorics.erl
@@ -1,6 +1,7 @@
 -module(ealgo_combinatorics).
 -include("ealgo.hrl").
 -export([cartesian_product/2, cartesian_product/1, cartesian_power/2]).
+-export([subsets/2, subsets/1]).
 
 
 %% https://en.wikipedia.org/wiki/Cartesian_product
@@ -26,4 +27,27 @@ cartesian_product([H | T]) when is_list(H) ->
 cartesian_power(L, N) when is_list(L), is_integer(N), ?GTE(N) ->
     cartesian_product(lists:duplicate(N, L)).
 
+
+%% Gives all subsets of L containing exactly N elements. 
+-spec subsets(L :: [term()], N :: non_neg_integer()) ->
+    R :: [list()].
+subsets(L, N) when is_list(L), is_integer(N), ?GTE(N) ->
+    case erlang:length(L) of
+        R when R > N -> [];
+        R -> subsets(L, N, R)
+    end.
+subsets(_, 0, _) -> [[ ]];
+subsets(L, N, N) -> [ L ];
+subsets([H | T], N, R) ->
+    A = subsets(T, N - 1, R - 1),
+    B = subsets(T, N    , R - 1),
+    [[H | E] || E <- A] ++ B.
+
+%% Gives all subsets of L. 
+-spec subsets(L :: [term()]) ->
+    R :: [list()].
+subsets(L) when is_list(L) ->
+    lists:append(lists:map(fun(N) ->
+        subsets(L, N)
+    end, lists:seq(0, erlang:length(L)))).
 
--- test/ealgo_combinatorics_tests.erl
+++ test/ealgo_combinatorics_tests.erl
@@ -1,5 +1,10 @@
 -module(ealgo_combinatorics_tests).
 -include_lib("eunit/include/eunit.hrl").
+-import(ealgo_combinatorics, [
+      cartesian_product/2
+    , cartesian_product/1
+    , cartesian_power/2
+]).
 
 
 all_test_() ->
@@ -7,11 +12,18 @@ all_test_() ->
         fun() -> error_logger:tty(false) end,
         fun(_) -> error_logger:tty(true) end,
         [
-              {<<"basic_operation">>, fun basic_operation/0}
+              {<<"test_cartesian">>, fun test_cartesian/0}
         ]
     }.
 
 
-basic_operation() ->
+test_cartesian() ->
+    ?assertEqual(cartesian_product([1, 2, 3, 4, 5, 6], []), []),
+    ?assertEqual(cartesian_product([a, b, c], [x, y, z]),
+        [[a,x], [a,y], [a,z], [b,x], [b,y], [b,z], [c,x], [c,y], [c,z]]),
+    ?assertEqual(cartesian_product([]), []),
+    ?assertEqual(cartesian_product([[a, b], [p, q], [x, y]]),
+        [[a,p,x], [a,p,y], [a,q,x], [a,q,y], [b,p,x], [b,p,y], [b,q,x], [b,q,y]]),
+    ?assertEqual(cartesian_power([0, 1], 2), [[0,0], [0,1], [1,0], [1,1]]),
     ok.
 
