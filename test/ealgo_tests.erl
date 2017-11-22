24aedc90a0270cd460e692b6394c11ba2a143dcb
dploop <dploop@163.com>
2017-11-22 12:08:44 +0800

    add test cases

--- test/ealgo_tests.erl
+++ test/ealgo_tests.erl
@@ -1,9 +1,12 @@
 -module(ealgo_tests).
 -include_lib("eunit/include/eunit.hrl").
 -import(ealgo, [
-      cartesian_product/2
-    , cartesian_product/1
-    , cartesian_power/2
+      cartesian_product/1
+    , combinations/1, combinations/2
+    , permutations/1, permutations/2
+    , next_permutation/1
+    , sign/1, boole/1, ustep/1
+    , rabin_karp/2, rabin_karp/3
 ]).
 
 
@@ -12,20 +15,84 @@ all_test_() ->
         fun() -> error_logger:tty(false) end,
         fun(_) -> error_logger:tty(true) end,
         [
-              {<<"basic_operation">>, fun basic_operation/0}
+            {<<"test cartesian product">>
+                , fun test_cartesian_product/0},
+            {<<"test combinations">>
+                , fun test_combinations/0},
+            {<<"test permutations">>
+                , fun test_permutations/0}
         ]
     }.
 
 
-basic_operation() ->
+test_cartesian_product() ->
+    ?assertEqual(cartesian_product([])
+                , [[]]),
+    ?assertEqual(cartesian_product([[]])
+                , []),
+    ?assertEqual(cartesian_product([[a]])
+                , [[a]]),
+    ?assertEqual(cartesian_product([[a, b, c]])
+                , [[a], [b], [c]]),
+    ?assertEqual(cartesian_product([[a, b, c], []])
+                , []),
+    ?assertEqual(cartesian_product([[], [a, b, c]])
+                , []),
+    ?assertEqual(cartesian_product([[a, b, c], [x, y, z]])
+                , [[a,x], [a,y], [a,z], [b,x], [b,y], [b,z], [c,x], [c,y], [c,z]]),
+    ?assertEqual(cartesian_product([[a, b], [p, q], [x, y]])
+                , [[a,p,x], [a,p,y], [a,q,x], [a,q,y], [b,p,x], [b,p,y], [b,q,x], [b,q,y]]),
     ok.
 
-test_cartesian() ->
-    ?assertEqual(cartesian_product([1, 2, 3, 4, 5, 6], []), []),
-    ?assertEqual(cartesian_product([a, b, c], [x, y, z]),
-        [[a,x], [a,y], [a,z], [b,x], [b,y], [b,z], [c,x], [c,y], [c,z]]),
-    ?assertEqual(cartesian_product([]), []),
-    ?assertEqual(cartesian_product([[a, b], [p, q], [x, y]]),
-        [[a,p,x], [a,p,y], [a,q,x], [a,q,y], [b,p,x], [b,p,y], [b,q,x], [b,q,y]]),
-    ?assertEqual(cartesian_power([0, 1], 2), [[0,0], [0,1], [1,0], [1,1]]),
+
+test_combinations() ->
+    ?assertEqual(combinations([])
+                , [[]]),
+    ?assertEqual(combinations([a])
+                , [[], [a]]),
+    ?assertEqual(combinations([a, b])
+                , [[], [b], [a], [a, b]]),
+    ?assertEqual(combinations([a, b, c])
+                , [[], [c], [b], [b, c], [a], [a, c], [a, b], [a, b, c]]),
+    ?assertEqual(combinations([], 0)
+                , [[]]),
+    ?assertEqual(combinations([], 1)
+                , []),
+    ?assertEqual(combinations([a, b, c], 0)
+                , [[]]),
+    ?assertEqual(combinations([a, b, c], 1)
+                , [[a], [b], [c]]),
+    ?assertEqual(combinations([a, b, c], 2)
+                , [[a, b], [a, c], [b, c]]),
+    ?assertEqual(combinations([a, b, c], 3)
+                , [[a, b, c]]),
+    ?assertEqual(combinations([a, b, c], 4)
+                , []),
     ok.
+
+
+test_permutations() ->
+    ?assertEqual(permutations([])
+                , [[]]),
+    ?assertEqual(permutations([a])
+                , [[a]]),
+    ?assertEqual(permutations([a, b])
+                , [[a, b], [b, a]]),
+    ?assertEqual(permutations([a, b, c])
+                , [[a,b,c], [a,c,b], [b,a,c], [b,c,a], [c,a,b], [c,b,a]]),
+    ?assertEqual(permutations([], 0)
+                , [[]]),
+    ?assertEqual(permutations([], 1)
+                , []),
+    ?assertEqual(permutations([a, b, c], 0)
+                , [[]]),
+    ?assertEqual(permutations([a, b, c], 1)
+                , [[a], [b], [c]]),
+    ?assertEqual(permutations([a, b, c], 2)
+                , [[a, b], [a, c], [b, a], [b, c], [c, a], [c, b]]),
+    ?assertEqual(permutations([a, b, c], 3)
+                , [[a,b,c], [a,c,b], [b,a,c], [b,c,a], [c,a,b], [c,b,a]]),
+    ?assertEqual(permutations([a, b, c], 4)
+                , []),
+    ok.
+
